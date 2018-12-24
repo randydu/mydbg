3473f08017d942dcb3a798d86efe47dddcdab47a
randy <randydu@gmail.com>
2018-12-23 16:45:08 -0800

    + DEBUG preprocessing;

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# mydbg
-Simple wrapper of nodejs debug module with debug levels
+mydbg
+=====
+
+Simple wrapper of nodejs [debug module](https://www.npmjs.com/package/debug)  with debug level control.
+
+Install
+-------
+
+```sh
+npm install mydbg
+```
+
+Usage
+------
+
+```javascript
+
+const dbg = require('mydbg')('app');
+
+dbg.info('app starting...');
+
+try {
+    ...
+}catch(ex){
+    dbg.err(ex.message);
+}
+
+```
+
+Levels
+-------
+
+There are 5 levels of debug messages supported:
+
+- trace: detailed messages for development
+
+- info: generic information messages
+
+- warn: something abnormal detected, but app can still run
+
+- err: error detected, but app can still run
+
+- fatal: error detected, app must terminate
+
+The importance of levels are sorted as following:
+
+```sh
+trace < info < warn < err < fatal
+```
+
+Output Control
+--------------
+
+The output of debug messages can be controlled by its level in DEBUG environment variable
+
+```sh
+DEBUG=app.warn,app.err,app.fatal
+```
+
+or simply use '+' to specify that all messages of the same or high levers are included:
+
+```sh
+DEBUG=app.warn+
+```
+
+To enable warn messages of all app modules:
+
+```sh
+DEBUG=*.warn
+```
+
+To enable messages starts from warn level:
+```sh
+DEBUG=*.warn+
+```
+
+To enable messages for multiple moduels:
+
+```sh
+DEBUG=app.warn+,srv.err+,-ui.*
+```
+
+Please refer to [debug module](https://www.npmjs.com/package/debug) for *Wildcards*
+
+Methods
+--------
+
+- trace() / info() / warn() / err() / fatal()
+
+They are message output api for each debug level. please refer to [debug module](https://www.npmjs.com/package/debug) for *Formatters*
+
+```js
+
+const dbg = require('mydbg')('srv');
+
+dbg.trace('server starts');
+```
+
+- throw_err(message)
+
+outputs the message as *err* level and throw an *Error* instance.
+
+```js
+try {
+    ...
+}catch(ex){
+    dbg.throw_err(ex.message);
+}
+```
--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 'use strict'
 
-const dbg = require('debug');
+
+const levels = ['trace','info','warn','err','fatal'];
+
+/**  parse environment variable DEBUG
+ * 
+ * xxx.trace+ => xxx.trace,xxx.info,xxx.warn,xxx.err,xxx.fatal 
+ * xxx.info+ => xxx.info,xxx.warn,xxx.err,xxx.fatal 
+ * xxx.warn+ => xxx.warn,xxx.err,xxx.fatal 
+ * xxx.err+ => xxx.err,xxx.fatal 
+ * xxx.fatal+ => xxx.fatal 
+ */
+(function (str){
+    function get_replace_str(matched, level){
+        let xxx_dot = matched.substring(0, matched.length - level.length-1); //including "."
+        let i = levels.indexOf(level);
+        return levels.slice(i).map(level => `${xxx_dot}${level}`).join();
+    }
+
+    if(str != ''){
+        let pats = []
+        levels.forEach( level => {
+            pats.push(new RegExp(`[a-zA-Z0-9\*]*\.${level}\\+`));
+        })
+
+        for(let i = 0; i < levels.length; i++){
+            let level = levels[i];
+            let pat = pats[i];
+
+            let my_str = pat.exec(str);
+            while(my_str != null && my_str.length > 0){
+                str = str.replace(my_str, get_replace_str(my_str[0], level));
+                my_str = pat.exec(str);
+            }
+        }
+
+        process.env.DEBUG = str;
+    }
+})(process.env.DEBUG)
 
 module.exports = (name)=>{
-        return {
-            trace: dbg(`${name}.trace`),
-            info: dbg(`${name}.info`),
-            warn: dbg(`${name}.warn`),
-            err: dbg(`${name}.err`),
-            fatal: dbg(`${name}.fatal`),
-            throw_error(msg){
-                this.err(msg);
-                throw new Error(msg);
-            },
-        };
-    }    
+    const dbg = require('debug');
+
+    let result = {
+        throw_error(msg){
+            this.err(msg);
+            throw new Error(msg);
+        }
+    }
+
+    levels.forEach(level => {
+        result[level] = dbg(`${name}.${level}`);
+    });
+
+    return result;
+}
